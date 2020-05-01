525ed85b46e651e0483cef351368898a39a8258f
rcaglar <rukal.61@gmail.com>
2020-04-30 22:18:40 -0400

    Configuration class is updated

--- src/test/java/com/vytrack/utilities/ConfigurationReader.java
+++ src/test/java/com/vytrack/utilities/ConfigurationReader.java
@@ -7,9 +7,8 @@ import java.util.Properties;
 //ConfigurationReader.configFile.getProperty("browser") direct access
 //ConfigurationReader.getProperty("browser")
 public class ConfigurationReader {
-    //this class will be responsible for loading properties file and will provide access
-    //to values based on key names
-    //we use Properties class to load custom .properties files
+ 
+    //I use Properties class to load custom .properties files
     private static Properties configFile;
 
     static {
@@ -18,7 +17,7 @@ public class ConfigurationReader {
             //try/catch block stands for handling exceptions
             //if exception occurs, code inside a catch block will be executed
             //any class that is related to InputOutput produce checked exceptions
-            //without handling checked exception, you cannot run a code
+    
             FileInputStream fileInputStream = new FileInputStream("configuration.properties");
             //initialize properties object
             configFile = new Properties();
